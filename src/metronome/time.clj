7398e7f5e1e19125b61678208b5dc666b43988d3
richardsunny2015 <richardsunny2015@gmail.com>
2021-08-08 17:17:22 -0400

    Add parse-time-str fn

--- src/metronome/time.clj
+++ src/metronome/time.clj
@@ -1,13 +1,44 @@
-(ns metronome.time)
+(ns metronome.time
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]
+            [clojure.string :as cstr]))
 
-(defn calculate-seconds
+;; Not sure what these are for yet. I have them jic.
+(defn- calculate-seconds
   "Takes a number of seconds and returns the seconds
    left in a minute."
   [seconds]
   (mod seconds 60))
 
-(defn calculate-minutes
+(defn- calculate-minutes
   "Takes a number of seconds and returns the amount of minutes
    you can make with those seconds."
   [seconds]
   (quot seconds 60))
+
+(defn- parse-int
+  [int]
+  (Integer/parseInt int))
+
+(defn parse-time-str
+  "Takes a string formatted as `MM:SS`
+   and returns the total seconds."
+  [time-str]
+  (let [[minutes seconds] (->> (cstr/split time-str #":")
+                               (map parse-int))]
+    (+ (* 60 minutes)
+       seconds)))
+
+(defn- time-str?
+  [time-str]
+  (and (string? time-str)
+       (not (nil? (re-matches #"\d\d:\d\d" time-str)))))
+
+(s/def ::seconds number?)
+(s/def ::time-str time-str?)
+
+(s/fdef parse-time-str
+  :args (s/cat :time-str ::time-str)
+  :ret ::seconds)
+
+(stest/instrument `parse-time-str)
