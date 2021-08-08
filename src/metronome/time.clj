897b05e49b3ec1ada5ad244eb748f822e0637a6f
richardsunny2015 <richardsunny2015@gmail.com>
2021-08-08 17:47:36 -0400

    Add calculate-interval fn

--- src/metronome/time.clj
+++ src/metronome/time.clj
@@ -3,22 +3,16 @@
             [clojure.spec.test.alpha :as stest]
             [clojure.string :as cstr]))
 
-;; Not sure what these are for yet. I have them jic.
-(defn- calculate-seconds
-  "Takes a number of seconds and returns the seconds
-   left in a minute."
-  [seconds]
-  (mod seconds 60))
-
-(defn- calculate-minutes
-  "Takes a number of seconds and returns the amount of minutes
-   you can make with those seconds."
-  [seconds]
-  (quot seconds 60))
-
 (defn- parse-int
   [int]
   (Integer/parseInt int))
+
+(defn- time-str?
+  [time-str]
+  (and (string? time-str)
+       (not (nil? (re-matches #"\d\d:\d\d" time-str)))))
+
+;; API
 
 (defn parse-time-str
   "Takes a string formatted as `MM:SS`
@@ -29,16 +23,23 @@
     (+ (* 60 minutes)
        seconds)))
 
-(defn- time-str?
-  [time-str]
-  (and (string? time-str)
-       (not (nil? (re-matches #"\d\d:\d\d" time-str)))))
+(defn calculate-interval
+  [seconds frequency]
+  (/ seconds frequency))
 
 (s/def ::seconds number?)
+(s/def ::frequency pos-int?)
+(s/def ::interval pos?)
 (s/def ::time-str time-str?)
 
 (s/fdef parse-time-str
   :args (s/cat :time-str ::time-str)
   :ret ::seconds)
 
-(stest/instrument `parse-time-str)
+(s/fdef calculate-interval
+  :args (s/cat :seconds ::seconds
+               :frequency ::frequency)
+  :ret ::seconds)
+
+(stest/instrument `parse-time-str
+                  `calculate-interval)
