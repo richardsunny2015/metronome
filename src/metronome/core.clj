acdff8b980b30ecde97e253fa0837cf808732e68
richardsunny2015 <richardsunny2015@gmail.com>
2021-08-08 12:21:17 -0400

    Remove test fn

--- src/metronome/core.clj
+++ src/metronome/core.clj
@@ -1,11 +1,6 @@
 (ns metronome.core
   (:require [metronome.sounds :refer [play-beep]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn -main
   []
   (play-beep))
